5bdeec34b12cd01dd709ecbe94d7f7bc45008de0
UmerFaroq <umerfaroq@gmail.com>
2024-02-05 18:01:46 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'image-viewer-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ImageViewerLibrary';
+}
